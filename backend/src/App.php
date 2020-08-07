872dbe7a01b7313b89f0443c567fca0637bccbcd
Enieber Cunha <eniebercunha@gmail.com>
2020-08-07 04:38:10 -0300

    refactor: update php and libs

--- backend/src/App.php
+++ backend/src/App.php
@@ -2,33 +2,35 @@
 
 namespace Blog;
 
-use \Slim\App as Server;
-use \Psr\Http\Message\ServerRequestInterface as Request;
-use \Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Slim\Factory\AppFactory;
 
 use Blog\Author;
 use Blog\Visitante;
+
 
 class App
 {
   public function run()
   {
-    $server = new Server;
-    $server->get('/posts', function (Request $request, Response $response, array $args) {
- 	echo "posts";
+    $app = AppFactory::create();
+
+    $app->get('/posts', function (Request $request, Response $response, array $args) {
+ 	    echo "posts";
     });
-    $server->get('/', function (Request $request, Response $response, array $args) {
+
+    $app->get('/', function (Request $request, Response $response, array $args) {
       $visitante = new Visitante();
       $comentario = $visitante->comentar("Teste");
-      $newResponse = $response->withJson(array(0 => $comentario));
-      return $newResponse;
-    /*	    
-      $author = new Author();
-      $data = $author->insertAuthor('name', 'lastname', 'name@contact');
-      $newResponse = $response->withJson($data);
-      return $newResponse;
-      */
+
+      $data = array(0 => $comentario);
+      $payload = json_encode($data);
+      
+      $response->getBody()->write($payload);
+      return $response->withHeader('Content-Type', 'application/json');
     });
-    $server->run();
+
+    $app->run();
   }
 }
